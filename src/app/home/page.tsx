12605d010af7dc546dda47e693fbef04ea2b44a6
gitdevjin <dev.hjshin@gmail.com>
2025-11-03 13:05:24 -0500

    first PR

---
+++ src/app/home/page.tsx
@@ -0,0 +1,18 @@
+export default function HomePage() {
+    return <div className="">
+        
+        <div className="flex flex-row justify-between bg-gray-200">
+            <div className="text-lg">SplitMate</div>
+            <div className="flex flex-row gap-2">
+                <div>icon1</div>
+                <div>icon2</div>
+            </div>
+        </div>
+        <div className="text-2xl border-b-1 border-gray-500">Welcome Susan</div>
+        <div className="border-b-1 border-gray-500">Recent Transactions</div>
+        <div className="h-52 bg-gray-300">...</div>
+        <div>Groups</div>
+        <div className="h-56 bg-gray-300">Group Section</div>
+        
+    </div>
+}
